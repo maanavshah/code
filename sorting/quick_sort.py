d8ce1fbf02aafb08dbfd4d1fe57429b56ba01209
maanav <shah.maanav.07@gmail.com>
2021-04-06 20:30:18 +0530

    Add quick sort

---
+++ sorting/quick_sort.py
@@ -0,0 +1,17 @@
+def quickSort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        return quickSort(less) + equal + quickSort(greater)
+    else:
+        return array
