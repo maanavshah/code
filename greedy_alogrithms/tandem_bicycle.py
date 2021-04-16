134b023e1cfadc0b26b444de8723d21d6bba372d
maanav <shah.maanav.07@gmail.com>
2021-04-16 13:33:14 +0530

    Add tandem_bicycle

---
+++ greedy_alogrithms/tandem_bicycle.py
@@ -0,0 +1,12 @@
+def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+    totalLength = len(redShirtSpeeds)
+    redShirtSpeeds.sort()
+    blueShirtSpeeds.sort()
+    totalSpeed = 0
+    if fastest:
+        for i in range(totalLength):
+            totalSpeed += max(redShirtSpeeds[i], blueShirtSpeeds[totalLength - i - 1])
+    else:
+        for i in range(totalLength):
+            totalSpeed += max(redShirtSpeeds[i], blueShirtSpeeds[i])
+    return totalSpeed
